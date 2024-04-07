8edecc8c0856ebd26e99b4f09739cd605cb05232
Zaman <zamanofficial1980@gmail.com>
2024-04-08 00:29:39 +0530

    commit some changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [color, setColor] = useState("grey")
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactnhigkdfsjhjcgkh
-        </a>
-      </header>
+    <div className='bg-slate-700 w-full h-screen'>
+      <div className="flex flex-wrap gap-1 bg-slate-200 rounded-lg p-3 border-2 border-black-100 py-4">
+        <button className='bg-red-500 px-3 rounded-lg'
+        onClick={()=> setColor('red')}
+        >Red</button>
+      </div>
     </div>
   );
 }
